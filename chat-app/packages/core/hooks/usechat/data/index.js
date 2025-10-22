706e4343a0e37bce1c84d41157dd7a905ee49204
Santosnr6 <jesper-nyberg@hotmail.com>
2025-10-22 22:28:04 +0200

    added lecture code

---
+++ chat-app/packages/core/hooks/usechat/data/index.js
@@ -0,0 +1,27 @@
+import { useState, useRef } from 'react';
+
+export const useChatLogic = () => {
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const inputRef = useRef();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const question = inputRef.current.value;
+        if (!question.trim()) return;
+
+        setLoading(true);
+        setMessages((prev) => [...prev, { text: question, role: 'user' }]);
+        inputRef.current.value = '';
+        // const answer = await chain.invoke({question});
+
+        setMessages((prev) => [
+            ...prev,
+            { role: 'assistant', text: answer || 'Ingen respons.' },
+        ]);
+
+        setLoading(false);
+    };
+
+    return { messages, loading, handleSubmit, inputRef };
+};
